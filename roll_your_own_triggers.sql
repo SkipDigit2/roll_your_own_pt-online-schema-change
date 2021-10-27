b15f81afbe93d84aa6e3f77f4f7639893b296de1
Skipdigit2 <sherrikalm@gmail.com>
2021-10-27 11:11:14 -0700

    ready for test

--- roll_your_own_triggers.sql
+++ roll_your_own_triggers.sql
@@ -2,12 +2,11 @@
 -- ----------------------------------------------------- 
 -- INSERT TRIGGER 
 -- ----------------------------------------------------- 
-DROP TRIGGER IF EXISTS tr_production_customer_blink_insert_a
+DROP TRIGGER IF EXISTS tr_production_customer_blink_insert_a;
 
 DELIMITER | 
 
 CREATE 
-DEFINER = 'test_migration'
 TRIGGER tr_production_customer_blink_insert_a
 AFTER INSERT ON production.customer_blink
 
@@ -15,20 +14,21 @@ FOR EACH ROW
 
 BEGIN 
 
-REPLACE INTO new_customer_blink
+REPLACE INTO schema_change.new_customer_blink
 (customer_blink_id ,
 customer_id, 
 region_id ,
 blink_count, 
 date_inserted
 )
-VALUES 
+VALUES (
 NEW.customer_blink_id, 
 NEW.customer_id, 
 NEW.region_id, 
 NEW.blink_count, 
 NEW.date_inserted 
 ) 
+;
 
 END; 
 | 
@@ -38,20 +38,19 @@ DELIMITER ;
 -- ----------------------------------------------------- 
 -- UPDATE TRIGGER 
 -- ----------------------------------------------------- 
-DROP TRIGGER IF EXISTS tr_production_customer_blink_update_a
+DROP TRIGGER IF EXISTS tr_production_customer_blink_update_a;
 
 DELIMITER | 
 
 CREATE 
-DEFINER = 'test_migration'
-TRIGGER tr_production_customer_blink_insert_a
+TRIGGER tr_production_customer_blink_update_a
 AFTER UPDATE ON production.customer_blink
 
 FOR EACH ROW  
 
 BEGIN 
 
-REPLACE INTO new_customer_blink
+REPLACE INTO schema_change.new_customer_blink
 (
 customer_blink_id,
 customer_id, 
@@ -65,8 +64,7 @@ NEW.region_id,
 NEW.blink_count, 
 NEW.date_inserted
 )
-WHERE 
-    customer_blink_id = NEW.customer_blink_id ;
+;
 
 END; 
 | 
@@ -75,12 +73,11 @@ DELIMITER ;
 -- ----------------------------------------------------- 
 -- DELETE TRIGGER  - ok I don't condone hard deletes, but if you really must.. 
 -- ----------------------------------------------------- 
-DROP TRIGGER IF EXISTS tr_production_customer_blink_delete_a
+DROP TRIGGER IF EXISTS tr_production_customer_blink_delete_a;
 
 DELIMITER | 
 
 CREATE 
-DEFINER = 'test_migration'
 TRIGGER tr_production_customer_blink_delete_a
 AFTER DELETE ON production.customer_blink
 
@@ -88,9 +85,9 @@ FOR EACH ROW
 
 BEGIN 
 
-DELETE IGNORE FROM new_customer_blink 
+DELETE IGNORE FROM schema_change.new_customer_blink 
 WHERE 
-    customer_blink_id <=> NEW.customer_blink_id ;
+    customer_blink_id <=> OLD.customer_blink_id ;
 
 END; 
 | 
